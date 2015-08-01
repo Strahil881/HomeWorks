381288159201c959e84f36463d887e4527136c44
Страхил Васев Горанов <strahil_goranov@abv.bg>
2015-08-01 20:52:48 +0300

    Homework
    Homework

--- PHP_FOREACH_WHILE/zadacha_3.php
+++ PHP_FOREACH_WHILE/zadacha_3.php
@@ -1,11 +1,11 @@
 <?php
-$text=array('In' => 1,'mathematics' => 2, 'a' => 3 , 'real' => 4 , 'is' => 5, 'a' => 6, 'value' => 7 , 'that' => 8, 'represents' => 9, 'a' => 10, 'quantity' => 11, 'along' => 12, 'a' => 13, 'continuous' => 14,'line'=>15,'.'=>16, );
+$text=array('In' => 1,'mathematics' => 2, 'a' => 3 , 'real' => 4 , 'is' => 5, ' a ' => 6, 'value' => 7 , 'that' => 8, 'represents' => 9, ' a ' => 10, 'quantity' => 11, 'along' => 12, ' a ' => 13, 'continuous' => 14,'line'=>15,'.'=>16, );
 foreach( $text as $k => $v){
 	if($v%2==0){
-		echo '<span style="color=green">'. "$k" .'</span>';
+		echo '<span style="color:green">'. "$k" .'</span>';
 		echo " ";
 	}else{
-		echo '<span style="color=red">' ." $k " . '</span>';
+		echo '<span style="color:red">' ." $k " . '</span>';
 		echo " ";
 
 }
